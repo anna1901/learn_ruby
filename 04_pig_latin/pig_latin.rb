ceccd7dc50134a6164917ff6548b42345f5794fa
Anna Olak <anna.olak1901@gmail.com>
2019-07-19 23:17:34 +0200

    A way better solution of pig lain exercise

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,20 +1,20 @@
 def translate (text)
   text = text.split
   vowels = %w[a e i o u y]
-  consonantSoundsofTwo = %w[ch gh ph qu sc sh th wh kn ps wr br]
-  consonantSoundsofThree = %w[squ sch thr]
+
   textPigLatin = text.map do |word|
     if vowels.include?(word[0])
       word + "ay"
-    elsif consonantSoundsofThree.include?(word[0..2])
-      a = word[0..2]
-      word[3..word.length] + a + "ay"
-    elsif consonantSoundsofTwo.include?(word[0..1])
-      a = word[0..1]
-      word[2..word.length] + a + "ay"
     else
-      a = word[0]
-      word[1..word.length] + a + "ay"
+      i = 1
+      until vowels.include?(word[i])
+        i+=1
+      end
+      if word[i-1..i] == "qu"
+        word[i+1..word.length] + word[0..i] + "ay"
+      else
+        word[i..word.length] + word[0..i-1] + "ay"
+      end
     end
   end
   textPigLatin.join(" ")
