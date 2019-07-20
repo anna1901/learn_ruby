5ebf298036c8e29095448e0e6e810bf05492db0c
Anna Olak <anna.olak1901@gmail.com>
2019-07-20 15:58:04 +0200

    Done the book exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+  attr_reader :title
+
+  def title= (title)
+    @title = titleize(title)
+  end
+
+  def titleize(text)
+    words = text.split
+    arr = ["of", "and", "over", "by", "in", "on", "the", "a", "an"]
+    words.each {|word| word.capitalize! unless arr.include?(word) }
+    words[0].capitalize!
+    words.join(" ")
+  end
 end
