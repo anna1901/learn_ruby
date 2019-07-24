2c34206564f99963883181aee3e07b4f098427dc
Anna Olak <anna.olak1901@gmail.com>
2019-07-24 18:40:06 +0200

    Solved timer exercise

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours_string = @seconds/(60*60)
+    minutes_string =  (@seconds-hours_string*60*60)/60
+    seconds_string = @seconds % 60
+    "#{padded(hours_string)}:#{padded(minutes_string)}:#{padded(seconds_string)}"
+  end
+
+  private
+  def padded(number)
+    number<10 ? "0#{number}" : number
+  end
+
 end
